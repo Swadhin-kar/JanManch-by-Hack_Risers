6c12d1f991fa33472c49f465132d3d926aea2b75
RAJ KUMAR NATH SHARMA <rajkumarnathsharma@gmail.com>
2025-09-08 01:32:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -368,10 +368,11 @@ app.post('/law/:id/generate-report', isLoggedInAndAdmin, async (req, res) => {
             }))
         };
         
-        const pythonProcess = spawn('python3', [
-            path.join(__dirname , 'sentiment_api', 'sentiment_api.py'),
-            JSON.stringify(payload)
-        ]);
+        // This is the first code block
+const pythonProcess = spawn('python3', [
+    path.join(__dirname, 'sentiment_api', 'sentiment_api.py'),
+    JSON.stringify(payload)
+   ]);
 
         let result = '';
         let pythonError = '';
